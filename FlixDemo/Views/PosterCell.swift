8e344299a9ec9bffd97e613b97621a78426d4723
Leslie <Leslie@Mayas-MacBook-Pro-2.local>
2018-10-02 02:37:02 -0400

    finished collection view layout

---
+++ FlixDemo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixDemo
+//
+//  Created by Leslie  on 10/2/18.
+//  Copyright © 2018 Leslie . All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}

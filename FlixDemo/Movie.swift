7b81fcb91fa5df55f5b278a89485ec51204aabf4
Leslie <Leslie@Mayas-MacBook-Pro-2.local>
2018-10-18 15:13:36 -0400

    week 5 lab finished

---
+++ FlixDemo/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  FlixDemo
+//
+//  Created by Leslie  on 10/18/18.
+//  Copyright © 2018 Leslie . All rights reserved.
+//
+
+import Foundation
---
+++ FlixDemo/ViewControllers/MovieApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  MovieApiManager.swift
+//  FlixDemo
+//
+//  Created by Leslie  on 10/18/18.
+//  Copyright © 2018 Leslie . All rights reserved.
+//
+
+import Foundation
